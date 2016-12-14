4187c7aa67ee2dfd744b50c8178da1cdee2f0538
Gus <gus@littlebigbyte.com>
2016-12-14 20:06:26 +1100

    First Commit

---
+++ light_sensor.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+__author__ = 'Gus (Adapted from Adafruit)'
+__license__ = "GPL"
+__maintainer__ = "pimylifeup.com"
+
+GPIO.setmode(GPIO.BOARD)
+
+#define the pin that goes to the circuit
+pin_to_circuit = 7
+
+def rc_time (pin_to_circuit):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+#Catch when script is interupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        print rc_time(pin_to_circuit)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
