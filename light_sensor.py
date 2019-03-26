197534d40e5334cb53705841ac015d654a6b222c
Emmet Young <death-droid@hotmail.com>
2019-03-26 15:08:25 +1100

    Improve compatibility with Python 3
    Change the print statement to the print() function as required by Python 3

--- light_sensor.py
+++ light_sensor.py
@@ -33,7 +33,7 @@ def rc_time (pin_to_circuit):
 try:
     # Main loop
     while True:
-        print rc_time(pin_to_circuit)
+        print(rc_time(pin_to_circuit))
 except KeyboardInterrupt:
     pass
 finally:
